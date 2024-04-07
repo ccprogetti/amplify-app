eb987f2d195738e18de8a0a705f176e03096378c
ccprogetti <andrea.cavalieri.1@gmail.com>
2024-04-07 09:37:20 +0200

    recupero custom attribute sample

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,14 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "amplifyapp9cb06599": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
-      "HostedUIDomain": "string",
-      "OAuthMetadata": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Amplify } from 'aws-amplify';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import { getCurrentUser, FetchUserAttributesOutput, UserAttributeKey } from 'aws-amplify/auth';
+import * as auth from 'aws-amplify/auth';
+
 
 @Component({
   selector: 'app-root',
@@ -12,4 +15,32 @@ import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 })
 export class AppComponent {
   title = 'amplify-app';
+  user = currentAuthenticatedUser();
+  attributes = auth.fetchUserAttributes();
+
+}
+
+async function currentAuthenticatedUser() {
+  try {
+
+    const user = await auth.getCurrentUser();
+
+    console.log(user);
+
+    const  userAttributes  = await auth.fetchUserAttributes();
+    
+    //let attributes: FetchUserAttributesOutput = userAttributes as FetchUserAttributesOutput;
+
+    console.log(`The attributes: ${userAttributes['custom:tenant_id']}`);
+    
+
+    const { username, userId, signInDetails } = await getCurrentUser();
+
+    console.log(`The username: ${username}`);
+    console.log(`The userId: ${userId}`);
+    console.log(`The signInDetails: ${signInDetails}`);
+    console.log(`The email: ${userAttributes}`);
+  } catch (err) {
+    console.log(err);
+  }
 }
